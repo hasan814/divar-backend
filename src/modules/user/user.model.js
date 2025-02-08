90dfa0ec880de8f54cb0cc3576c4405b5f536b3d
Hasan Moosavi <h.mousavi910@gmail.com>
2025-02-08 11:52:16 +0330

    Added User Model

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,34 @@
+import { Schema, model } from "mongoose";
+
+const OTPSchema = new Schema({
+  code: { type: String, required: false, default: undefined },
+  expiresIn: { type: Number, required: false, default: 0 },
+});
+
+const userSchema = new Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    mobile: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    otp: { type: OTPSchema },
+    verifiedMobile: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
